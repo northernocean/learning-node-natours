da0612a95359b0049cf86f43504c2f14212886a6
David Pecot <4504352+northernocean@users.noreply.github.com>
2021-07-05 14:37:09 -0400

    add post request for new tour

--- app.js
+++ app.js
@@ -1,25 +1,58 @@
-const app = require('express')();
+const fs = require('fs');
+const express = require('express');
+const app = express();
+
+// Middleware for receiving JSON data in requests
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 const PORT = process.env.PORT || 3000;
 
-// Sending a Text Response
-app.get('/text', (req, res) => {
+// ------
+// Routes
+// ------
+
+app.get('/api/v1/tours', (req, res) => {
   res
     .status(200)
-    .send('Hello\n');
+    .json({
+      status: 'success',
+      data: { tours }
+    });
 });
 
-// Sending a Json Response
-app.get('/json', (req, res) => {
-  res
-    .status(200)
-    .json({message: 'Hello'});
+// To test with curl: curl -X POST -d "$(<foo.json)" -H 'Content-type: application/json' 127.0.0.1:3000/api/v1/tours
+app.post('/api/v1/tours', (req, res) => {
+  if (req) {
+    id = tours.map(c => c.id).sort((a, b) => a - b)[tours.length - 1] + 1;
+    newTour = Object.assign({"id": id}, req.body);
+    tours.push(newTour);
+    fs.writeFile(
+      `${__dirname}/dev-data/data/tours-simple.json`,
+      JSON.stringify(tours),
+      err => { 
+        if(err){
+          res
+            .status(500)
+            .json({"status": "error", "message": "internal error: unable to save data"})
+        }
+        else{
+          res
+            .status(201)
+            .json({
+              "status": "success",
+              "data": newTour
+            });
+          }
+      })
+  }
 });
 
-app.post("/post", (req, res) => {
-  res
-    .send('Testing a post method!\n');
-});
+// --------------
+// Go Express App
+// --------------
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
